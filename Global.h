db72bc58f78242bc7fceecf4b6d2cb18d329d705
REO MIYA <41591rionmiyazaki@gmail.com>
2023-01-27 02:42:42 +0900

    Inputのクラス化(初期化)

--- Global.h
+++ Global.h
@@ -16,12 +16,6 @@ using namespace DirectX;
 #include <d3dcompiler.h>
 #pragma comment(lib,"d3dcompiler.lib")
 
-#define DIRECTINPUT_VERSION 0x0800	// DirectInputのバージョン指定
-#include <dinput.h>
-
-#pragma comment(lib, "dinput8.lib")
-#pragma comment(lib, "dxguid.lib")
-
 // スケーリング倍率
 XMFLOAT3 scale;
 // 回転角
---
+++ Input/Input.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <windows.h>
+
+// “ü—Í
+class Input
+{
+public: // ƒƒ“ƒoŠÖ”
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize(HINSTANCE hInstance, HWND hwnd);
+	/// <summary>
+	/// XV
+	/// </summary>
+	void Update();
+
+private:
+
+};
